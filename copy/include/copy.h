ce508b1d15b17b1d1591653897549e161a67d62a
Emanuel Targino <emanueltargino1@gmail.com>
2021-06-24 11:22:14 -0300

    resposta copy

---
+++ copy/include/copy.h
@@ -0,0 +1,29 @@
+#ifndef GRAAL_H
+#define GRAAL_H
+
+#include <utility>
+using std::pair;
+#include <iterator>
+using std::distance;
+#include <algorithm>
+using std::sort;
+
+namespace graal {
+
+/*! 
+ * TODO: documentação no estilo doxygen
+ */
+template<class InputIt>
+InputIt copy(InputIt first, InputIt last, InputIt d_first)
+{
+    for (InputIt i = first; i != last; i++)
+    {
+        *d_first = *i;
+        d_first++;
+    }
+    return d_first;
+    
+}
+
+}
+#endif
