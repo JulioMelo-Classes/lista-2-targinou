8491e544afbccc97796efb69fb6aaa9ed3bd6b53
Emanuel Targino <emanueltargino1@gmail.com>
2021-06-24 14:17:26 -0300

    reverse incompleto

---
+++ reverse/include/reverse.h
@@ -0,0 +1,31 @@
+#ifndef GRAAL_H
+#define GRAAL_H
+
+#include <utility>
+using std::pair;
+#include <iterator>
+using std::distance;
+#include <algorithm>
+using std::sort;
+
+namespace graal {
+
+/*! 
+ * TODO: documentação no estilo doxygen
+ */
+template<class BidirIt>
+void reverse(BidirIt first, BidirIt last)
+{
+    BidirIt j = last;
+    j--;
+    for (BidirIt i = first; i != last; i++)
+    {
+        *first = *j;
+        first++;
+        j--;
+        
+    }
+}
+
+}
+#endif
