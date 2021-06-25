23d783767c3a63abceb5a0fab48aa2e21124d955
Emanuel Targino <emanueltargino1@gmail.com>
2021-06-25 17:30:41 -0300

    resposta partition

--- partition/include/partition.h
+++ partition/include/partition.h
@@ -8,6 +8,7 @@ using std::distance;
 #include <algorithm>
 using std::sort;
 
+
 namespace graal {
 
 /*! 
@@ -16,22 +17,29 @@ namespace graal {
 template<class ForwardIt, class UnaryPredicate>
 ForwardIt partition(ForwardIt first, ForwardIt last, UnaryPredicate p)
 {
-    ForwardIt retorno;
     int aux;
-    for (ForwardIt i = first; i != last; i++)
-    {
-        if(p(*i) == true){
-            retorno = i;
-            
-            aux = *first;
-            *first = *i;
-            *i = aux;
+    while (first!=last) {
+        while (p(*first)) {
             first++;
+            if (first==last){
+                return first;
+            }  
         }
+        do {
+            last--;
+            if (first==last){
+                return first;
+            } 
+        } 
+        while (!p(*last));
+        aux = *first;
+        *first = *last;
+        *last = aux;
+        first++;
     }
 
-    return retorno;
-}
+  return first;
 
 }
 #endif
+}
