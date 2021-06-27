4a1a775f78008422d388e176c29cb0072b62004d
Emanuel Targino <emanueltargino1@gmail.com>
2021-06-27 14:52:11 -0300

    documentação find_if

--- find_if/include/find_if.h
+++ find_if/include/find_if.h
@@ -11,7 +11,14 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam InputIt iterator para o range.
+ * @tparam UnaryPredicate o tipo de um função bool.
+ *
+ * @param first Ponteiro para o primeiro elemento do range.
+ * @param last Ponteiro para a posição logo após o último elemento do range.
+ * @param p Uma função que retorna true quando uma determinada condição é satisfeita ou falso caso contrário.
+ * 
+ * @return Um iterator apontando para o primeiro elemento do range que satisfaz p.
  */
 template<class InputIt, class UnaryPredicate>
 InputIt find_if(InputIt first, InputIt last, UnaryPredicate p)
